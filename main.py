2ed67b3744d3932999d01807437b46865515eff1
OliviaYCX <141985615+OliviaYCX@users.noreply.github.com>
2023-08-11 14:52:41 +0800

    abc

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
+import datetime
+
 class Customer:
     def __init__(self, customer_id, name, initial_balance):
-        self.customer_id = customer_id
-        self.name = name
-        self.account = Account(initial_balance)
+        self.__customer_id = customer_id
+        self.__name = name
+        self.__account = Account(initial_balance)
+
+    def get_customer_id(self):
+        return self.__customer_id
+
+    def get_name(self):
+        return self.__name
+
+    def get_account(self):
+        return self.__account
+
+
 
 # Main program
 if __name__ == "__main__":
     # Handle input and output here
     pass
+
+
+
+
+
