47c7088590f26ae93779b4c727cd6c98f73df346
Shaldiya2 <140777771+Shaldiya2@users.noreply.github.com>
2023-08-11 14:00:42 +0800

    START

--- main.py
+++ main.py
@@ -1,25 +1,6 @@
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
 class ShowBalance:
     def
 
 
 class WithdrawMoney:
     def
-
-
-
-=======
->>>>>>> 0573c94899dbb8e575503b3a3584ec4454bc1862
