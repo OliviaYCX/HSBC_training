70f00845175afe4bb1da12e5b7430672bc18c385
Shaldiya2 <140777771+Shaldiya2@users.noreply.github.com>
2023-08-11 14:16:46 +0800

    Customer Class
    Initializing the Customer Accounts

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-class ShowBalance:
-    def
+class Customer:
+    def __init__(self, customer_id, name, initial_balance):
+        self.customer_id = customer_id
+        self.name = name
+        self.account = Account(initial_balance)
 
-
-class WithdrawMoney:
-    def
+# Main program
+if __name__ == "__main__":
+    # Handle input and output here
+    pass
