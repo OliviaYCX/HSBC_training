de1ba0ebc72a79cc10e114417dd70aa8cc1a4227
Shaldiya2 <140777771+Shaldiya2@users.noreply.github.com>
2023-08-11 15:07:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,14 +15,36 @@ class Customer:
     def get_account(self):
         return self.__account
 
+class Account:
+    def __init__(self, initial_balance):
+        self.__account_id = generate_account_id()  # Private method to generate account ID
+        self.__balance = initial_balance
+        self.__transactions = []
 
+    def __add_transaction(self, transaction):
+        self.__transactions.append(transaction)
+        if len(self.__transactions) > 10:
+            self.__transactions.pop(0)
+
+    def withdraw(self, amount):
+        return BankModule.withdraw(self, amount)
+
+    def deposit(self, amount):
+        return BankModule.deposit(self, amount)
+
+    def transfer(self, destination_account, amount):
+        return BankModule.transfer(self, destination_account, amount)
+
+    def get_balance(self):
+        return self.__balance
+
+    def last_10_transactions(self):
+        return self.__transactions
+
+    def get_account_id(self):
+        return self.__account_id
 
 # Main program
 if __name__ == "__main__":
     # Handle input and output here
     pass
-
-
-
-
-
